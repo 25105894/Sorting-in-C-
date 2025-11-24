e515a926b083a63d193f73a20b19c74e7e2f7ede
Nitesh <nohabagla@gmail.com>
2025-11-25 00:45:39 +0530

    Add files via upload

---
+++ bubble shot sorting in descending order.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int arr[1000];
+   int n;
+   cout<<"Enter the size of an arr: ";
+   cin>>n;
+   cout<<"Enter the numbers of arr: ";
+   for(int i = 0;i < n;i++)
+    {
+        cin>>arr[i];
+    }
+      for(int i = n - 1;i >= 0;i--)
+       {
+          for(int j = 0;j<= i;j++)
+          {
+             if(arr[j] < arr[j + 1])
+
+               swap(arr[j],arr[j + 1]);
+            }
+
+
+       }
+
+        for(int i = 0;i < n;i++)
+        {
+            cout<<arr[i]<<" ";
+
+        }
+
+
+}
